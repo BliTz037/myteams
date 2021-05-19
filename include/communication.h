cbd146ff872060ce3371c0bf444c6f4c0f04d629
Alan Sigal <alan.sigal@epitech.eu>
2021-05-19 16:12:26 +0200

    [ADD] unions of stucture for communication

--- include/communication.h
+++ include/communication.h
@@ -12,23 +12,141 @@
 
 typedef enum command
 {
-    USE,
+    LOGIN,
+    LOGOUT,
+    USER,
+    SEND,
+    MESSAGES,
+    SUBSCRIBE,
+    SUBSCRIBED,
+    UNSUBSCRIBE,
+    CREATE,
+    LIST,
+    INFO
 } command;
+
+typedef struct login_s
+{
+    char username[MAX_NAME_LENGTH];
+} login_t;
+
+typedef struct user_s
+{
+    int user_uuid;
+} user_t;
+
+typedef struct send_s
+{
+    int user_uuid;
+    char body[MAX_BODY_LENGTH];
+} send_t;
+
+typedef struct message_s
+{
+    int user_uuid;
+} message_t;
+
+enum subcribe
+{
+    SUBSCRIBE,
+    SUBSCRIBED,
+    UNSUBSCRIBE
+};
+
+typedef struct subscribe_s
+{
+    int team_uuid;
+    subcribe subcribe_type;
+} subscribe_t;
+
+typedef struct teams_manipulation_s
+{
+    char team_name[MAX_NAME_LENGTH];
+    char team_description[MAX_DESCRIPTION_LENGTH];
+} teams_manipulation_t;
+
+typedef struct channel_manipulation_s
+{
+    char channel_name[MAX_NAME_LENGTH];
+    char channel_description[MAX_DESCRIPTION_LENGTH];
+    int team_uuid;
+} create_teams_t;
+
+typedef struct thread_manipulation_s
+{
+    char thread_title[MAX_NAME_LENGTH];
+    char thread_message[MAX_BODY_LENGTH];
+    int team_uuid;
+    int channel_uuid;
+} thread_teams_t;
+
+typedef struct message_manipulation_s
+{
+    char body[MAX_BODY_LENGTH];
+    int team_uuid;
+    int channel_uuid;
+    int thread_uuid;
+} message_teams_t;
+
+enum service_type
+{
+    TEAMS,
+    CHANNEL,
+    THREAD,
+    MESSAGE,
+};
+
+typedef struct create_s
+{
+    union
+    {
+        teams_manipulation_s teams;
+        channel_manipulation_s channel;
+        thread_manipulation_s thread;
+        message_manipulation_s messasge;
+    };
+    service_type type;
+} create_t;
+
+typedef struct list_s
+{
+    union
+    {
+        teams_manipulation_s teams;
+        channel_manipulation_s channel;
+        thread_manipulation_s thread;
+        message_manipulation_s messasge;
+    };
+    service_type type;
+} list_t;
+
+typedef struct info_s
+{
+    union
+    {
+        teams_manipulation_s teams;
+        channel_manipulation_s channel;
+        thread_manipulation_s thread;
+        message_manipulation_s messasge;
+    };
+    service_type type;
+} info_t;
 
 typedef struct request_s
 {
     union
     {
-        char name[MAX_NAME_LENGTH];
-        char body[MAX_BODY_LENGTH];
-        char description[MAX_DESCRIPTION_LENGTH];
-        int team_uuid;
+        login_t login;
+        user_t user;
+        send_t send;
+        message_t message;
+        subscribe_t subcribe;
+        create_t create;
+        list_t list;
+        info_t info;
     };
     int code;
     command command;
-    int user_uuid;
-    int channel_uuid;
-    int thread_uuid;
 } request_t;
 
 typedef struct response_s

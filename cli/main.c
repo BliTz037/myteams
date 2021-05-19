e41c6bc1bebf6f43b6180f3e9620e1b81700abd3
Valentin Macé <valentin.mace@epitech.eu>
2021-05-19 20:07:05 +0200

    add

--- cli/main.c
+++ cli/main.c
@@ -6,6 +6,7 @@
 */
 
 #include "cli.h"
+#include "communication.h"
 
 int print_help(char *av)
 {
@@ -16,11 +17,45 @@ int print_help(char *av)
     return (0);
 }
 
+char *get_command_line(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t lineSize = 0;
+
+    printf(">");
+    lineSize = getline(&line, &len, stdin);
+    if (lineSize == -1)
+        return NULL;
+    return (line);
+}
+
+int fill_request_struct(char *command, response_t *msg)
+{
+
+}
+
+int send_message(request_t msg)
+{
+
+}
+
+int client_loop(void)
+{
+    request_t msg;
+
+    while (1) {
+        if (fill_request_struct(get_command_line(), &msg) != NULL)
+            send_message(msg);
+    }
+}
+
 int main(int ac, char **av)
 {
     if (ac != 2)
         return (84);
     if (strcmp(av[1], "-help") == 0)
         return print_help(av[0]);
+    client_loop();
     return (0);
 }
--- include/cli.h
+++ include/cli.h
@@ -10,5 +10,6 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #endif /* !CLI_H_ */

ec0e05e7d2c951ffe08927bbf4d0d5293e62d961
Tom Rives <tom.rives@epitech.eu>
2021-05-22 23:58:50 +0200

    [FIX] main arguments

--- cli/main.c
+++ cli/main.c
@@ -29,9 +29,9 @@ void display(request_t *msg)
 
 int main(int ac, char **av)
 {
+    if (ac == 2 && strcmp(av[1], "-help") == 0)
+        return print_help(av[0]);
     if (ac != 3)
         return (84);
-    if (strcmp(av[1], "-help") == 0)
-        return print_help(av[0]);
     return client_loop(av[1], atoi(av[2]));
 }

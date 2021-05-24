cd1abc530723543fc03ffe26746ecc896c6403e8
Tom Rives <tom.rives@epitech.eu>
2021-05-24 23:28:57 +0200

    [EDIT] reduce number of teams / channel / threads / comments

--- include/teams.h
+++ include/teams.h
@@ -12,10 +12,10 @@
 #define MAX_DESCRIPTION_LENGTH 255
 #define MAX_BODY_LENGTH 512
 #define MAX_CLIENTS 30
-#define MAX_TEAMS 50
-#define MAX_CHANNEL 50
-#define MAX_THREADS 50
-#define MAX_COMMENTS 50
+#define MAX_TEAMS 10
+#define MAX_CHANNEL 10
+#define MAX_THREADS 10
+#define MAX_COMMENTS 10
 #define UUID_SIZE 36
 
 #endif /* !TEAMS_H_ */

d2eff891f41264dda1bd386c6763572085438580
Tom Rives <tom.rives@epitech.eu>
2021-05-27 00:11:16 +0200

    [ADD][CLI] Manage response command subscribed

--- cli/response/subscribed.c
+++ cli/response/subscribed.c
@@ -6,12 +6,34 @@
 */
 
 #include "communication.h"
+#include "../libs/myteams/logging_client.h"
 #include "cli.h"
+
+static void print_users(response_t *rcv)
+{
+    for (int i = 0; i != MAX_CLIENTS; i++) {
+        if (rcv->subscribed.users[i].name[0] != '\0')
+            client_print_users(rcv->subscribed.users[i].uuid,
+            rcv->subscribed.users[i].name,
+            rcv->subscribed.users[i].status);
+    }
+}
+
+static void print_teams(response_t *rcv)
+{
+    for (int i = 0; i != MAX_TEAMS; i++) {
+        if (rcv->subscribed.teams[i].team_uuid[0] != '\0')
+            client_print_teams(rcv->subscribed.teams[i].team_uuid,
+            rcv->subscribed.teams[i].team_name,
+            rcv->subscribed.teams[i].team_description);
+    }
+}
 
 void response_subscribed(cli_t *cli, response_t *rcv)
 {
     (void)(cli);
-    (void)(rcv);
-    printf("MATCH SUBSCRIBED\n");
-    return;
+    if (rcv->subscribed.teams[0].team_name[0] != '\0')
+        print_teams(rcv);
+    else
+        print_users(rcv);
 }

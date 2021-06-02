b1ba365bb7fd0d61dff371c04cc4d7db557dd4d1
Alan Sigal <alan.sigal@epitech.eu>
2021-06-02 14:09:16 +0200

    [ADD] logout response when leaving withouyt /logout

--- server/server_handling/server_user_connection.c
+++ server/server_handling/server_user_connection.c
@@ -7,15 +7,29 @@
 
 #include "server.h"
 #include "logging_server.h"
+#include "communication.h"
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void close_connection(server_t *server, int fd, int client)
 {
+    response_t *response = malloc(sizeof(response_t));
+
+    response->code = 200;
+    response->command = LOGOUT;
+    strcpy(response->user.users[0].name, server->clients[client].name);
+    memcpy(response->user.users[0].uuid, server->clients[client].uuid, UUID_SIZE);
     close(fd);
     server->clients[client].socket = 0;
     if (server->clients[client].loged == 1)
         server_event_user_logged_out(server->clients[client].uuid);
     server->clients[client].loged = -1;
+    for (int i = 0; i != MAX_CLIENTS; i++)
+    {
+        if (server->clients[i].loged == 1)
+            write(server->clients[i].socket, response, sizeof(response_t));
+    }
+    free(response);
 }
